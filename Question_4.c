bca9d9c65cc27d80ea9e7e54f218a5009f610493
The1bain <101262117+The1bain@users.noreply.github.com>
2022-03-15 18:14:53 +0100

    Add files via upload

---
+++ Question_4.c
@@ -0,0 +1,40 @@
+
+#include <sys/types.h>
+
+#include <md5.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[])
+
+{
+
+  char buffer[1000];
+
+  if (argc != 2)
+   {
+
+	printf ("Le nombre de parametres n'est pas respecte");
+  
+	exit(1);
+   }
+
+else 
+	{
+
+		unsigned char *var=NULL;
+
+		var=MD5File(argv[1], buffer);
+
+		printf("le code MD5 est: %s %s \n", var, argv[1] );
+
+	}
+
+
+}
+
+
---
+++ Question_6.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+
+#include <md5.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[])
+
+	{
+
+	     char fich[1000];
+
+	     char buffer[1000];
+
+	     unsigned char * var=NULL;
+
+
+	     int i=1;
+	     
+while (i<=1000)
+	     
+{
+
+		printf("Nom du fichier (%d) : ", i+1);
+
+		scanf("%s",fich);
+var=MD5File(fich, buffer);
+
+		printf("le code MD5 est: %s %s \n", var, fich );
+		
+i=i+1;
+
+	     }
+
+	
+} 
